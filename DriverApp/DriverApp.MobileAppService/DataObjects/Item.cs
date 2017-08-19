c99ffbae601aba10e78b780a74c55e5b331d8f3e
vicky.5sept <vicky.5sept@gmail.com>
2017-08-19 20:57:12 +0200

    added driver app

---
+++ DriverApp/DriverApp.MobileAppService/DataObjects/Item.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Azure.Mobile.Server;
+
+namespace DriverApp.MobileAppService.DataObjects
+{
+    public class Item : EntityData
+    {
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
